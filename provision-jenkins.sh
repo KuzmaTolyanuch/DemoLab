9615125d687fa2d7de77cf564a51286d80e2eea6
Valerii Kuzmenko <vaken111@gmail.com>
2018-07-09 20:45:31 +0300

    Update provision-jenkins.sh

--- provision-jenkins.sh
+++ provision-jenkins.sh
@@ -1,1 +1,47 @@
+#!/bin/bash
 
+########################
+# Jenkins & Java
+########################
+echo "Installing Jenkins and Java"
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
+
+########################
+# Docker
+########################
+echo "Installing Docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+
+########################
+# nginx
+########################
+echo "Installing nginx"
+sudo apt-get -y install nginx > /dev/null 2>&1
+sudo service nginx start
+
+########################
+# Configuring nginx
+########################
+echo "Configuring nginx"
+cd /etc/nginx/sites-available
+sudo rm default ../sites-enabled/default
+sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+sudo service nginx restart
+sudo service jenkins restart
+
+########################
+# Saving Jenkins Initial Pass
+########################
+sudo cp /var/lib/jenkins/secrets/initialAdminPassword /mnt/host_machine/JenkinsPass.txt
+echo "Success"
