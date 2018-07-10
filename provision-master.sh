74aab2e5e6aeb82bd24ec7ac0e5d7a39f4c9789f
Valerii Kuzmenko <vaken111@gmail.com>
2018-07-10 16:20:15 +0300

    Update provision-master.sh

--- provision-master.sh
+++ provision-master.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# This script will install Jenkins, Java, Docker, Git, and Asible.
+# This script will install Jenkins, Java, Maven, Docker, Git, and Asible.
 
 #################################
-# Jenkins & Java
+# Jenkins & Java setup
 #################################
 echo "Installing Jenkins and Java"
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -y install oracle-java8-installer
 
 #################################
 # Docker
@@ -31,7 +34,7 @@ sudo apt-get -y install nginx > /dev/null 2>&1
 sudo service nginx start
 
 #################################
-# Configuring nginx
+# Configuring nginx for Jenkins
 #################################
 echo "Configuring nginx"
 cd /etc/nginx/sites-available
@@ -74,6 +77,7 @@ sudo apt-get install -y python-setuptools
 sudo easy_install jinja2 
 sudo easy_install pyyaml
 sudo easy_install paramiko
+sudo apt-get install make
 
 #################################
 # Install Ansible
@@ -108,5 +112,39 @@ echo '[Install Ansible] Export ANSIBLE_HOST path'
 export ANSIBLE_HOSTS=/etc/ansible/hosts
 
 echo '[Install Ansible] Done!'
+
+#################################
+# Maven install
+#################################
+set -e
+
+# script to install maven
+
+# todo: add method for checking if latest or automatically grabbing latest
+mvn_version=${mvn_version:-3.5.2}
+url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
+install_dir="/opt/maven"
+
+if [ -d ${install_dir} ]; then
+    mv ${install_dir} ${install_dir}.$(date +"%Y%m%d")
+fi
+
+mkdir ${install_dir}
+curl -fsSL ${url} | tar zx --strip-components=1 -C ${install_dir}
+
+cat << EOF > /etc/profile.d/maven.sh
+#!/bin/sh
+export MAVEN_HOME=${install_dir}
+export M2_HOME=${install_dir}
+export M2=${install_dir}/bin
+export PATH=${install_dir}/bin:$PATH
+EOF
+
+source /etc/profile.d/maven.sh
+
+echo maven installed to ${install_dir}
+mvn --version
+
+printf "\n\nTo get mvn in your path, open a new shell or execute: source /etc/profile.d/maven.sh\n"
 
 echo "Success"
